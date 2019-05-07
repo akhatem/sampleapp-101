46a03c94eccea47738cb981741562c94197b1ad9
kareem shawadfy <ahmed.kareem.elshawadfy@gmail.com>
2019-05-07 17:07:31 +0200

    sendgrid

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'carrierwave'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'rename'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,26 @@ User.create!(name:  "Kareem User",
              activated_at: Time.zone.now,
              image: image_admin)
 
+User.create!(name:  "Hatem ElShawadfy",
+            email: "hatem@admin.com",
+            password: "123456",
+            password_confirmation: "123456",
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now,
+            image: Rails.root.join("app/assets/images/profileImg.jpeg").open)
+
+User.create!(name:  "Kareem User",
+             email: "kareem@admin.me",
+             password: "Asdasd",
+             password_confirmation: "Asdasd",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now,
+             image: Rails.root.join("app/assets/images/default-img.jpeg").open)
+
+
+
 99.times do |n|
   name  = Faker::Name.name
   email = "test-#{n+1}@example.com"
